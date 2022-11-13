6b3f3ec5619104a35ff98065e2b08cdb9b107788
Leonid_Rusak <116496436+LeonidRusak@users.noreply.github.com>
2022-11-13 14:59:30 +0300

    Add files via upload

--- Rusak_104_lesson20.py
+++ Rusak_104_lesson20.py
@@ -3,7 +3,7 @@ card_rang = ['туз', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'валет
 card_suit = ['треф', 'бубен', 'червей', 'пик']
 
 card_deck = [i + ' ' + x for i in card_rang for x in card_suit]  # Список содежащий название всех 52х карт в коложе
-cardDeck = iter(card_deck)
+cardDeck = iter(card_deck)  # Итератор колоды карт
 
 print(cardDeck)
 
