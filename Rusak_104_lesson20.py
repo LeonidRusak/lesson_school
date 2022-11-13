26f398e34980ee132a388e3ce9d29c965ecf00be
Leonid_Rusak <116496436+LeonidRusak@users.noreply.github.com>
2022-11-13 14:57:02 +0300

    Add files via upload

---
+++ Rusak_104_lesson20.py
@@ -0,0 +1,11 @@
+# Домашнее задание
+card_rang = ['туз', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'валет', 'дама', 'король']
+card_suit = ['треф', 'бубен', 'червей', 'пик']
+
+card_deck = [i + ' ' + x for i in card_rang for x in card_suit]  # Список содежащий название всех 52х карт в коложе
+cardDeck = iter(card_deck)
+
+print(cardDeck)
+
+for i in range(53):  # Проверка циклом, выполняется ли условие задания
+    print(next(cardDeck))
